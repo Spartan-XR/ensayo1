60d52232331f748d54580a323c713735a0852065
Spartan-XR <iasparausar816@gmail.com>
2024-11-06 11:56:42 -0600

    Update limpiador.js

--- js/limpiador.js
+++ js/limpiador.js
@@ -1,4 +1,7 @@
-document.getElementById('contactar').addEventListener('submit', function(event) { 
-    event.preventDefault(); 
-    this.reset(); 
+window.addEventListener('beforeunload', function() { 
+    sessionStorage.setItem('formState', 'leaving'); 
+}); // Limpiar el formulario al regresar a la página window.addEventListener('load', function() { 
+if (sessionStorage.getItem('formState') === 'leaving') { 
+    document.getElementById('contact-form').reset(); 
+    sessionStorage.removeItem('formState'); } 
 });
